1bbaac57c6b35c3eab304970cf8c625a1420878c
Steven Rivera <ghostszmusic@gmail.com>
2019-03-26 16:25:12 -0500

    angular added

--- functions.php
+++ functions.php
@@ -9,7 +9,13 @@ function ghs_scrs(){
     wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/style.css');
 
     //js scripts
-    wp_enqueue_script('mainJS', get_stylesheet_directory_uri() . '/js/GhsJS.js', array(), true);
+
+    wp_enqueue_script('jq', get_stylesheet_directory_uri() . '/js/jquery-2.1.3.js');
+    wp_enqueue_script('angularjs', get_stylesheet_directory_uri() . '/js/angular.min.js');
+    wp_enqueue_script('angularjs-resource', get_stylesheet_directory_uri() . '/js/angular-resource.min.js');
+    wp_enqueue_script('angularjs-route',  get_stylesheet_directory_uri() . '/js/angular-route.min.js');
+    wp_enqueue_script('cookies',  get_stylesheet_directory_uri() . '/js/cookies.js');
+    wp_enqueue_script('GhsJS', get_stylesheet_directory_uri() . '/js/GhsJS.js', array( 'angularjs', 'angularjs-route', 'angularjs-resource', 'jq', 'cookies'), true);
 
     //localization of scripts
     wp_localize_script('GhsJS', 'myLocalized', array('partials' => trailingslashit( get_template_directory_uri() ) . 'partials/'));
@@ -19,7 +25,7 @@ function head(){ ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="google-signin-client_id" content="979177383237-vevih7vk4k8niblajr70uu1qfbesapt0.apps.googleusercontent.com">
-    <link rel="shortcut icon" type="image/x-icon" href="https://ghostszmusic.com/wp-content/uploads/2016/03/favicon.ico" />
+<!--    <link rel="shortcut icon" type="image/x-icon" href="https://ghostszmusic.com/wp-content/uploads/2016/03/favicon.ico" />-->
     <base href="<?php echo site_url('/'); ?>">
 <?php
 }
